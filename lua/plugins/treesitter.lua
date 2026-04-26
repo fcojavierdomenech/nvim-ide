87d5e20538a7ef94b550ee922e50aec2e7667ba8
Javi Domenech <fcojavierdomenech@gmail.com>
2026-04-26 11:41:11 +0200

    chore: treesitter more compatible

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,68 +1,44 @@
--- Treesitter configuration (Updated for nvim-treesitter rewrite)
+require('nvim-treesitter.configs').setup({
+    -- A list of parser names, or "all"
+    ensure_installed = {
+        'php',
+        'python',
+        'go',
+        'gomod',
+        'javascript',
+        'typescript',
+        'html',
+        'css',
+        'scss',
+        'json',
+        'yaml',
+        'lua',
+        'vim',
+        'vimdoc',
+        'query',
+        'bash',
+        'sql',
+        'dockerfile',
+        'vue',
+        'markdown',
+        'markdown_inline',
+    },
 
-local ts = require('nvim-treesitter')
+    -- Install parsers synchronously (only applied to `ensure_installed`)
+    sync_install = false,
 
-ts.setup({
-    -- You can specify install_dir if you want, but default is usually fine
-})
+    -- Automatically install missing parsers when entering buffer
+    auto_install = true,
 
--- Install parsers
-ts.install({
-    'php',
-    'python',
-    'go',
-    'gomod',
-    'javascript',
-    'typescript',
-    'html',
-    'css',
-    'scss',
-    'json',
-    'yaml',
-    'lua',
-    'vim',
-    'bash',
-    'sql',
-    'dockerfile',
-    'vue',
-    'markdown',
-    'markdown_inline',
-})
-
--- Enable highlighting for all supported languages
-vim.api.nvim_create_autocmd('FileType', {
-    callback = function()
-        -- Skip special buffers like Telescope, NvimTree, etc.
-        if vim.bo.buftype ~= "" then return end
-        
-        local lang = vim.treesitter.language.get_lang(vim.bo.filetype) or vim.bo.filetype
-        if lang == "" or lang:find("Telescope") then return end
-
-        local ok, _ = pcall(vim.treesitter.get_parser, 0, lang)
-        if ok then
-            local has_query = pcall(vim.treesitter.query.get, lang, 'highlights')
-            if has_query then
-                pcall(vim.treesitter.start, 0, lang)
-            end
-        end
-    end,
-})
-
--- Enable indentation
-vim.api.nvim_create_autocmd('FileType', {
-    callback = function()
-        -- Skip special buffers
-        if vim.bo.buftype ~= "" then return end
-
-        local lang = vim.treesitter.language.get_lang(vim.bo.filetype) or vim.bo.filetype
-        if lang == "" or lang:find("Telescope") then return end
-
-        local ok, _ = pcall(vim.treesitter.get_parser, 0, lang)
-        if ok then
-            local has_query = pcall(vim.treesitter.query.get, lang, 'indents')
-            if has_query then
-                vim.opt_local.indentexpr = "v:lua.require('nvim-treesitter').indentexpr()"
-            end
-        end
-    end,
+    highlight = {
+        enable = true,
+        -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+        -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+        -- Using this option may slow down your editor, and you may see some duplicate highlights.
+        -- Instead of true it can also be a list of languages
+        additional_vim_regex_highlighting = false,
+    },
+    indent = {
+        enable = true
+    },
 })
